f15de89480a353f93bfffc60776d8056fc3b90f9
jidp97 <jidp97@gmail.com>
2023-06-03 13:31:27 -0400

    first commit

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using Humanizer;
+using System.Globalization;
+
+
+Console.WriteLine("Quantities:");
+HumanizeQuantities();
+
+Console.WriteLine("\nDate/Time Manipulation:");
+HumanizeDates();
+
+static void HumanizeQuantities()
+{
+    Console.WriteLine("case".ToQuantity(0));
+    Console.WriteLine("case".ToQuantity(1));
+    Console.WriteLine("case".ToQuantity(5));
+}
+
+static void HumanizeDates()
+{
+    Console.WriteLine(DateTime.UtcNow.AddHours(-24).Humanize());
+    //en español
+    Console.WriteLine(DateTime.UtcNow.AddHours(-24).Humanize(culture: new CultureInfo("es-ES")));
+    //hace un año
+    Console.WriteLine(DateTime.UtcNow.AddYears(-1).Humanize());
+    Console.WriteLine(DateTime.UtcNow.AddHours(-2).Humanize());
+    Console.WriteLine(TimeSpan.FromDays(1).Humanize());
+    Console.WriteLine(TimeSpan.FromDays(16).Humanize());
+}
